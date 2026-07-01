f5a1ad4ce881f2dcc083115b1f4286037ebb6bd1
Bas Nijholt <bas@nijho.lt>
2026-06-30 20:21:59 -0700

    cli: Center web banner windows

--- src/compose_farm/cli/web.py
+++ src/compose_farm/cli/web.py
@@ -30,18 +30,18 @@ def _compose_farm_banner() -> Text:
     banner.append("|_|", style=attic_window)
     banner.append("    \\\n", style=structure)
     banner.append("      /-------------\\\n", style=structure)
-    banner.append("     /  ", style=structure)
+    banner.append("     /   ", style=structure)
     banner.append("[]", style=windows)
     banner.append("  ", style=structure)
     banner.append("[]", style=windows)
     banner.append("  ", style=structure)
     banner.append("[]", style=windows)
     banner.append("   \\\n", style=structure)
-    banner.append("    /_________________\\\n", style=structure)
-    banner.append("    |  ", style=structure)
+    banner.append("    /__________________\\\n", style=structure)
+    banner.append("    |   ", style=structure)
     banner.append("COMPOSE FARM", style="bold green")
     banner.append("   |\n", style=structure)
-    banner.append("    |_________________|", style=structure)
+    banner.append("    |__________________|", style=structure)
     return banner
 
 
